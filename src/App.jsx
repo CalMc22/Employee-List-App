e9b177e236dc17bddd77ce97240f2847350eb03d
Cal <cmcconnell104@gmail.com>
2023-12-11 18:38:40 -0500

    group work code commit to github

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Employee from "./pages/Employee";
+import Homepage from "./pages/Homepage"
+// import "./index.css"
+
+
+function App() {
+  
+  return (
+          <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/employee/:first_name" element={<Employee />} />
+      </Routes>
+   
+  );
+}
+
+export default App;
